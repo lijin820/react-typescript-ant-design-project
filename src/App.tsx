9dd4fb6870f80fb8785ed6584738df87357bce21
Li Jin <lijin1991.dev@gmail.com>
2021-01-07 18:43:55 +0800

    fixed header ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,139 +1,70 @@
-import React, { ReactElement, useState } from "react";
-import { Layout, Menu, Tabs, Divider } from "antd";
-import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+import React, { ReactElement } from "react";
+import { Layout, Menu, Tabs, Input } from "antd";
+import { SearchOutlined } from "@ant-design/icons";
 
 import ActivityTab from "./components/ActivityTab";
+
+import richard from "./assets/images/richard.jpg";
 import "./App.css";
 
 const { Header, Sider, Content } = Layout;
 const { TabPane } = Tabs;
 
 function App(): ReactElement {
-  const [collapsed, setCollapsed] = useState(false);
-
-  const toggleSidebar = () => {
-    setCollapsed(!collapsed);
-  };
-
   return (
     <Layout className="App">
-      <Sider
-        trigger={null}
-        collapsible
-        collapsed={collapsed}
-        width={250}
-        style={{ backgroundColor: "#3394fc" }}
-      >
+      <Sider trigger={null} width={250} style={{ backgroundColor: "#3394fc" }}>
         <div className="logo">
           <span>PERCHPEEK</span>
         </div>
-        <Divider
-          style={{
-            backgroundColor: "#55B4FF",
-            width: 210,
-            minWidth: 210,
-            margin: 20,
-          }}
-        />
         <Menu
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["1"]}
-          style={{ backgroundColor: "#3394fc" }}
+          className="custom-menu"
         >
-          <Menu.Item
-            key="1"
-            style={{
-              color: "#D1F7FB",
-              fontWeight: 500,
-              fontSize: 15,
-            }}
-          >
+          <Menu.Divider className="custom-divider" />
+          <Menu.Item key="1" className="custom-menu-item">
             Overview
           </Menu.Item>
-          <Divider
-            style={{
-              backgroundColor: "#55B4FF",
-              width: 210,
-              minWidth: 210,
-              margin: 20,
-            }}
-          />
-          <div className="section-title">
-            <span>MANAGE</span>
-          </div>
-          <Menu.Item
-            key="2"
-            style={{
-              color: "#D1F7FB",
-              fontWeight: 500,
-              fontSize: 15,
-            }}
-          >
-            Users
-          </Menu.Item>
-          <Menu.Item
-            key="3"
-            style={{
-              color: "#D1F7FB",
-              fontWeight: 500,
-              fontSize: 15,
-            }}
-          >
-            Properties
-          </Menu.Item>
-          <Menu.Item
-            key="4"
-            style={{
-              color: "#D1F7FB",
-              fontWeight: 500,
-              fontSize: 15,
-            }}
-          >
-            Viewings
-          </Menu.Item>
-          <Menu.Item
-            key="5"
-            style={{
-              color: "#D1F7FB",
-              fontWeight: 500,
-              fontSize: 15,
-            }}
-          >
-            Support
-          </Menu.Item>
-          <Divider
-            style={{
-              backgroundColor: "#55B4FF",
-              width: 210,
-              minWidth: 210,
-              margin: 20,
-            }}
-          />
-          <div className="section-title">
-            <span>GET HELP</span>
-          </div>
-          <Menu.Item
-            key="6"
-            style={{
-              color: "#D1F7FB",
-              fontWeight: 500,
-              fontSize: 15,
-            }}
-          >
-            Knowledge Base
-          </Menu.Item>
+          <Menu.Divider className="custom-divider" />
+          <Menu.ItemGroup title="MANAGE">
+            <Menu.Item key="2" className="custom-menu-item">
+              Users
+            </Menu.Item>
+            <Menu.Item key="3" className="custom-menu-item">
+              Properties
+            </Menu.Item>
+            <Menu.Item key="4" className="custom-menu-item">
+              Viewings
+            </Menu.Item>
+            <Menu.Item key="5" className="custom-menu-item">
+              Support
+            </Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.Divider className="custom-divider" />
+          <Menu.ItemGroup title="GET HELP">
+            <Menu.Item key="6" className="custom-menu-item">
+              Knowledge Base
+            </Menu.Item>
+          </Menu.ItemGroup>
         </Menu>
       </Sider>
       <Layout className="site-layout">
         <Header className="site-layout-background" style={{ padding: 0 }}>
-          {React.createElement(
-            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-            {
-              className: "trigger",
-              onClick: toggleSidebar,
-            }
-          )}
+          <Input
+            size="large"
+            style={{
+              width: "50%",
+              border: "none",
+              marginLeft: 16,
+            }}
+            placeholder="Search..."
+            prefix={<SearchOutlined />}
+          />
+          <div className="avatar-container">
+            <img src={richard} alt="avatar" className="profile-image" />
+          </div>
         </Header>
         <Content
           className="site-layout-background"
